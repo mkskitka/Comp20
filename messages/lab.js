c7786ed8e0cc43ce52591823e3997710e0506ac9
Mary Skitka <mary.skitka@tufts.edu>
2016-03-10 15:24:28 -0500

    functional, but need to fix error in console, undefined length

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,22 @@
-// Your JavaScript goes here...
+var request = new XMLHttpRequest();
+var url = "data.json";
+
+request.onreadystatechange = function() {
+    if (request.readyState == 4 && request.status == 200) {
+        var data = JSON.parse(request.responseText);
+        //console.log(data);
+        parse(data);
+    }
+};
+
+request.open("GET", url, true);
+request.send(null);
+
+function parse(data) {
+    var out = "";
+    var i;
+    for(i = 0; i < data.length; i++) {
+        out += data[i].content + " " + data[i].username + '<br/>';
+    }
+    document.getElementById("messages").innerHTML = out;
+}
