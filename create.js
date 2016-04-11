801e0e2defad41aa958e0b004c72581b49a08bb1
bpfand01 <benjamin.pfander@tufts.edu>
2016-04-11 16:05:26 -0400

    styled Create page

--- create.js
+++ create.js
@@ -14,7 +14,7 @@ var rowNum = 0;
 var rowNum = 0;
 function addRow(frm) {
 rowNum ++;
-var row = '<p id="rowNum'+rowNum+'">Amount: <input type="text" name="qty[]" size="4" value="'+frm.add_qty.value+'"> Ingredient: <input type="text" name="name[]" value="'+frm.add_name.value+'"> <input type="button" value="Remove" class="btn btn-default" onclick="removeRow('+rowNum+');"></p>';
+var row = '<p id="rowNum'+rowNum+'"><label>Amount</label> <input type="text" id=amount name="qty[]" size="4" value="'+frm.add_qty.value+'"> <label>Ingredient</label> <input type="text" name="name[]" value="'+frm.add_name.value+'"> <input type="button" value="Remove" class="btn btn-default" onclick="removeRow('+rowNum+');"></p>';
 jQuery('#itemRows').append(row);
 frm.add_qty.value = '';
 frm.add_name.value = '';
