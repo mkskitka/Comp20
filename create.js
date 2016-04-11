a8ba401271aed21966627d1fa905ae91d5ffb291
bpfand01 <benjamin.pfander@tufts.edu>
2016-04-11 01:58:46 -0400

    forgot my javascript file

---
+++ create.js
@@ -0,0 +1,10 @@
+// Script Language = JAVA
+
+var loadFile = function(event) {
+    var reader = new FileReader();
+    reader.onload = function(){
+      var output = document.getElementById('output');
+      output.src = reader.result;
+    };
+    reader.readAsDataURL(event.target.files[0]);
+};
