6c2921068192e4c5c5514f5098bd7ce17089ed7d
bpfand01 <benjamin.pfander@tufts.edu>
2016-04-11 15:44:20 -0400

    Ingredient Add Row and remove row

--- create.js
+++ create.js
@@ -8,3 +8,18 @@ var loadFile = function(event) {
     };
     reader.readAsDataURL(event.target.files[0]);
 };
+
+var rowNum = 0;
+
+var rowNum = 0;
+function addRow(frm) {
+rowNum ++;
+var row = '<p id="rowNum'+rowNum+'">Amount: <input type="text" name="qty[]" size="4" value="'+frm.add_qty.value+'"> Ingredient: <input type="text" name="name[]" value="'+frm.add_name.value+'"> <input type="button" value="Remove" class="btn btn-default" onclick="removeRow('+rowNum+');"></p>';
+jQuery('#itemRows').append(row);
+frm.add_qty.value = '';
+frm.add_name.value = '';
+}
+
+function removeRow(rnum) {
+jQuery('#rowNum'+rnum).remove();
+}
