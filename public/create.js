d7771ca594f6b62976f2b639ff4ac76b592220df
bpfand01 <benjamin.pfander@tufts.edu>
2016-04-30 15:08:38 -0400

    Create fixes, actually posts data

--- public/create.js
+++ public/create.js
@@ -4,7 +4,10 @@ var loadFile = function(event) {
     var reader = new FileReader();
     reader.onload = function(){
       var output = document.getElementById('output');
+      var image = document.getElementById('64bitImage');
+      console.log(reader.result);
       output.src = reader.result;
+      image.value = reader.result;
     };
     reader.readAsDataURL(event.target.files[0]);
 };
